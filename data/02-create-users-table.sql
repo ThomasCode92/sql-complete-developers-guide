0c315eb3420a72f8eb7e908482576d43bc04d863
ThomasCode92 <thomascode92@hotmail.com>
2023-10-04 08:10:06 +0200

    Working with Enums & Finishing Table Creation

--- data/02-create-users-table.sql
+++ data/02-create-users-table.sql
@@ -1,1 +1,9 @@
-CREATE TABLE users(full_name VARCHAR(255), yearly_salary INT);
+-- PostgreSQL & ENUM -> create a custom type first
+CREATE TYPE employment_status as ENUM('employed', 'self-employed', 'unemployed');
+-- Create Users Table
+CREATE TABLE users(
+  full_name VARCHAR(255),
+  yearly_salary INT,
+  current_status employment_status -- PostgreSQL syntax
+  -- current_status ENUM('employed', 'self-employed', 'unemployed') -- MySQL syntax
+);

4e04e0de766cc2d6755b725188b3d391f5d0ae23
ThomasCode92 <thomascode92@hotmail.com>
2023-10-16 08:36:05 +0200

    Time to Practice - Part 2

--- exercise/solution.sql
+++ exercise/solution.sql
@@ -17,3 +17,22 @@ VALUES (
     39,
     'uploads/images/products/a-book.jpg'
   );
+-- Task 5: Add constraints - MySQL
+ALTER TABLE products
+MODIFY COLUMN name VARCHAR(200) NOT NULL,
+MODIFY COLUMN price NUMERIC(10, 2) NOT NULL CHECK (price > 0),
+MODIFY COLUMN description TEXT NOT NULL,
+MODIFY COLUMN amount_in_stock SMALLINT CHECK (amount_in_stock >= 0);
+-- Task 5: Add constraints - PostgreSQL
+ALTER TABLE products
+ALTER COLUMN name SET NOT NULL,
+ALTER COLUMN price SET NOT NULL,
+ALTER COLUMN description SET NOT NULL,
+ADD CONSTRAINT price_positive CHECK (price > 0),
+ADD CONSTRAINT amount_in_stock_positive CHECK (price > 0);
+-- Task 6: Add id column - MySQL
+ALTER TABLE products
+ADD COLUMN id INT PRIMARY KEY AUTO_INCREMENT;
+-- Task 6: Add id column - PostgreSQL
+ALTER TABLE products
+ADD COLUMN id SERIAL PRIMARY KEY;

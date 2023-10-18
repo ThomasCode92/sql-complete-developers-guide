bed97b88c9023447c58661d824b8477a3cf8786d
ThomasCode92 <thomascode92@hotmail.com>
2023-10-18 20:55:16 +0200

    Ordering & Limiting Results in Action

---
+++ data/12-order-and-limit.sql
@@ -0,0 +1,12 @@
+SELECT * FROM sales
+ORDER BY volume DESC
+LIMIT 5;
+
+SELECT * FROM sales
+WHERE is_disputed IS FALSE
+ORDER BY volume DESC
+LIMIT 3;
+
+SELECT * FROM sales
+ORDER BY volume DESC
+LIMIT 5 OFFSET 3;

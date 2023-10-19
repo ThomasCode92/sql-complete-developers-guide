588301d118b880fbd941cbe6c30847bfe4de3f81
ThomasCode92 <thomascode92@hotmail.com>
2023-10-19 08:42:18 +0200

    Adding First Tables & Relations

---
+++ example/01-relationship.sql
@@ -0,0 +1,14 @@
+CREATE TABLE employees (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  birthday DATE NOT NULL,  
+  email VARCHAR(200) UNIQUE NOT NULL
+);
+
+CREATE TABLE intranet_accounts (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(200),
+  password VARCHAR(200) NOT NULL,
+  FOREIGN KEY (email) REFERENCES employees (email) ON DELETE CASCADE
+);

a742d4bdaf468c8474fe6697d840cfdeedf1d844
ThomasCode92 <thomascode92@hotmail.com>
2023-10-23 18:53:44 +0200

    Practicing JOINs with Filtering

--- example/09-query-data.sql
+++ example/09-query-data.sql
@@ -7,3 +7,17 @@ SELECT e.id AS employee_id, e.first_name, e.last_name, p.title
 FROM employees AS e
 INNER JOIN projects_employees AS pe ON pe.employee_id = e.id
 LEFT JOIN projects AS p ON pe.project_id = p.id;
+
+SELECT * FROM teams;
+
+SELECT e.id AS employee_id, e.first_name, e.last_name, t.name
+FROM employees AS e
+INNER JOIN teams AS t ON e.team_id = t.id
+WHERE t.id = 2;
+
+SELECT e.id AS employee_id, e.first_name, e.last_name, b.name
+FROM employees AS e
+INNER JOIN teams AS t ON e.team_id = t.id
+INNER JOIN company_buildings AS b ON t.building_id = b.id
+WHERE b.id = 3
+ORDER BY e.birthday;

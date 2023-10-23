3befa050c3572e6e39eebfed9d3e2db57348561f
ThomasCode92 <thomascode92@hotmail.com>
2023-10-23 18:52:15 +0200

    Adding More Tables & Data

---
+++ example/04-more-relations.sql
@@ -0,0 +1,28 @@
+CREATE TABLE company_buildings (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE teams (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(300) NOT NULL,
+  building_id INT,
+  FOREIGN KEY (building_id) REFERENCES company_buildings (id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  birthday DATE NOT NULL,  
+  email VARCHAR(200) UNIQUE NOT NULL,
+  team_id INT DEFAULT 1,
+  FOREIGN KEY (team_id) REFERENCES teams (id) ON DELETE SET DEFAULT 
+);
+
+CREATE TABLE intranet_accounts (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(200),
+  password VARCHAR(200) NOT NULL,
+  FOREIGN KEY (email) REFERENCES employees (email) ON DELETE CASCADE
+);

9e533eba25a016085bbed20ee11fe530f84c1000
ThomasCode92 <thomascode92@hotmail.com>
2023-10-24 20:37:15 +0200

    Understanding ORDER BY and RANK()

--- data/08-window-functions.sql
+++ data/08-window-functions.sql
@@ -3,3 +3,11 @@ FROM bookings;
 
 SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER(PARTITION BY booking_date)
 FROM bookings;
+
+SELECT booking_date, amount_tipped, SUM(amount_tipped) 
+OVER(PARTITION BY booking_date ORDER BY amount_billed)
+FROM bookings;
+
+SELECT booking_date, amount_tipped, RANK() 
+OVER(PARTITION BY booking_date ORDER BY amount_tipped DESC)
+FROM bookings;

3609d9550fbedc944189d19c4fdb1ebdd8567af8
ThomasCode92 <thomascode92@hotmail.com>
2023-10-24 21:02:47 +0200

    Working with Mathematical Functions & Arithmetic Operators

---
+++ data/01-mathematical-functions.sql
@@ -0,0 +1,10 @@
+SELECT price * billing_frequency AS annual_revenue
+FROM memberships;
+
+-- CEIL()
+-- FLOOR()
+-- ROUND()
+-- TRUNC() TRUNCATE() mysql
+
+SELECT TRUNC(consumption, 0) 
+FROM memberships;

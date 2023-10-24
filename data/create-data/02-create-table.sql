80120a62b3d9d89b0cc733ae3cc5c6fac550f166
ThomasCode92 <thomascode92@hotmail.com>
2023-10-24 21:02:42 +0200

    The Module Project

---
+++ data/create-data/02-create-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE memberships (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL syntax
+    -- id SERIAL PRIMARY KEY, --PostgreSQL syntax
+    membership_start DATE,
+    membership_end DATE,
+    last_checkin TIMESTAMP,
+    last_checkout TIMESTAMP,
+    consumption NUMERIC(5, 2),
+    first_name VARCHAR(200),
+    last_name VARCHAR(200),
+    price NUMERIC(5, 2),
+    billing_frequency INT,
+    gender VARCHAR(200)
+);

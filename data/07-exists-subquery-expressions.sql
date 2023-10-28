7e44df6319f4826c8d7c2aeb436b6e34ab31c142
ThomasCode92 <thomascode92@hotmail.com>
2023-10-28 15:26:09 +0200

    Working with Subquery Expressions and EXISTS

--- data/07-exists-subquery-expressions.sql
+++ data/07-exists-subquery-expressions.sql
@@ -7,3 +7,11 @@ SELECT EXISTS(
   FROM customers
   WHERE email = 'manu@test.com'
 );
+
+SELECT o.id
+FROM orders AS o
+WHERE EXISTS(
+  SELECT c.email
+  FROM customers as c
+  WHERE o.customer_id = c.id AND c.email = 'max@test.com'
+);

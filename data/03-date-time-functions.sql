58a098372385bfb617930dc3cad0d990bdf9f539
ThomasCode92 <thomascode92@hotmail.com>
2023-10-28 15:09:25 +0200

    Working with Weekdays

--- data/03-date-time-functions.sql
+++ data/03-date-time-functions.sql
@@ -1,2 +1,18 @@
 SELECT EXTRACT(MINUTE FROM last_checkin), last_checkin
 FROM memberships;
+
+-- MySQL syntax
+SELECT WEEKDAY(last_checkin) + 1, last_checkin 
+FROM memberships;
+
+-- PostgreSQL syntax
+SELECT EXTRACT(ISODOW FROM last_checkin), last_checkin 
+FROM memberships;
+
+-- MySQL syntax
+SELECT CONVERT(last_checkin, DATE), CONVERT(last_checkin, TIME)
+FROM memberships;
+
+-- PostgreSQL syntax
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
+FROM memberships;

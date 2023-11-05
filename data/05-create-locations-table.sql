c1f6a100802431f716de8740a8cc429e2bf44542
ThomasCode92 <thomascode92@hotmail.com>
2023-11-05 11:27:21 +0100

    Relations & Foreign Keys

--- data/05-create-locations-table.sql
+++ data/05-create-locations-table.sql
@@ -1,12 +1,29 @@
+CREATE TABLE cities (
+  name VARCHAR(255) PRIMARY KEY
+);
+
 CREATE TABLE locations (
   id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL syntax
   -- id SERIAL PRIMARY KEY, --PostgreSQL syntax
   title VARCHAR(255),
   street VARCHAR(255) NOT NULL,
   house_number VARCHAR(10) NOT NULL,
-  postal_code VARCHAR(5) NOT NULL
+  postal_code VARCHAR(5) NOT NULL,
+  city_name VARCHAR(255), -- MySQL syntax
+  FOREIGN KEY (city_name) REFERENCES cities (name) -- MySQL syntax
+  -- city_name VARCHAR(200) REFERENCES cities --PostgreSQL syntax
 );
 
-CREATE TABLE cities (
-  name VARCHAR(255) PRIMARY KEY
+CREATE TABLE events (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL syntax
+  -- id SERIAL PRIMARY KEY, --PostgreSQL syntax
+  name VARCHAR(255) CHECK (LENGTH(name) > 5) NOT NULL,
+  date_planned TIMESTAMP NOT NULL,
+  image VARCHAR(255),
+  description TEXT NOT NULL,
+  max_participants INT CHECK (max_participants > 0),
+  min_age INT CHECK (min_age > 0),
+  location_id INT, -- MySQL syntax
+  FOREIGN KEY (location_id) REFERENCES locations (id) -- MySQL syntax
+  -- location_id INT REFERENCES locations --PostgreSQL syntax
 );

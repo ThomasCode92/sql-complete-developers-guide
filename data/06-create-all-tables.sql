69d1b8f928b368746c4687972390000d7b272fcd
ThomasCode92 <thomascode92@hotmail.com>
2023-11-05 12:01:46 +0100

    Adding More Tables & Relationships

---
+++ data/06-create-all-tables.sql
@@ -0,0 +1,52 @@
+CREATE TABLE cities (
+  name VARCHAR(200) PRIMARY KEY
+);
+
+CREATE TABLE locations (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL syntax
+  -- id SERIAL PRIMARY KEY, --PostgreSQL syntax
+  title VARCHAR(255),
+  street VARCHAR(255) NOT NULL,
+  house_number VARCHAR(10) NOT NULL,
+  postal_code VARCHAR(5) NOT NULL,
+  city_name VARCHAR(255), -- MySQL syntax
+  FOREIGN KEY (city_name) REFERENCES cities (name) ON DELETE RESTRICT ON UPDATE CASCADE -- MySQL syntax
+  -- city_name VARCHAR(200) REFERENCES cities ON DELETE RESTRICT ON UPDATE CASCADE --PostgreSQL syntax
+);
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL syntax
+  -- id SERIAL PRIMARY KEY, --PostgreSQL syntax
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  birthday DATE NOT NULL,
+  email VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE organizers (
+  password VARCHAR(500) NOT NULL,
+  user_id INT PRIMARY KEY, -- MySQL syntax
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE -- MySQL syntax
+  -- user_id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE --PostgreSQL syntax
+);
+
+CREATE TABLE tags (
+  name VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE events (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL syntax
+  -- id SERIAL PRIMARY KEY, --PostgreSQL syntax
+  name VARCHAR(255) CHECK (LENGTH(name) > 5) NOT NULL,
+  date_planned TIMESTAMP NOT NULL,
+  image VARCHAR(255),
+  description TEXT NOT NULL,
+  max_participants INT CHECK (max_participants > 0),
+  min_age INT CHECK (min_age > 0),
+  location_id INT, -- MySQL syntax
+  organizer_id INT, -- MySQL syntax
+  FOREIGN KEY (location_id) REFERENCES locations (id) ON DELETE CASCADE, -- MySQL syntax
+  FOREIGN KEY (organizer_id) REFERENCES organizers (user_id) ON DELETE CASCADE -- MySQL syntax
+  -- location_id INT REFERENCES locations ON DELETE CASCADE, --PostgreSQL syntax
+  -- organizer_id INT REFERENCES organizers ON DELETE CASCADE --PostgreSQL syntax
+);

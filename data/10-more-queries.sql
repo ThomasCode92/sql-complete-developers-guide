65e0bb111584ff200a58e1a76dbdf5e0edf41b17
ThomasCode92 <thomascode92@hotmail.com>
2023-11-06 17:25:08 +0100

    The HAVING Clause (vs WHERE)

--- data/10-more-queries.sql
+++ data/10-more-queries.sql
@@ -6,3 +6,9 @@ SELECT COUNT(id) FROM locations;
 SELECT c.name, loc.street, COUNT(loc.id) AS num_locations FROM cities AS c
 LEFT JOIN locations AS loc ON loc.city_name = c.name
 GROUP BY c.name, loc.street;
+
+SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
+LEFT JOIN locations AS loc ON loc.city_name = c.name
+-- WHERE loc.city_name = 'Munich'
+GROUP BY c.name
+HAVING COUNT(loc.id) > 1;

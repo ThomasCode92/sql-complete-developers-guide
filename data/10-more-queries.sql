4e3aa2e26555b6e88b26af65fedb8a6baaf5519f
ThomasCode92 <thomascode92@hotmail.com>
2023-11-06 17:24:56 +0100

    Grouping Aggregate Results (GROUP BY)

--- data/10-more-queries.sql
+++ data/10-more-queries.sql
@@ -2,3 +2,7 @@ SELECT * FROM users
 WHERE first_name LIKE 'Ma%';
 
 SELECT COUNT(id) FROM locations;
+
+SELECT c.name, loc.street, COUNT(loc.id) AS num_locations FROM cities AS c
+LEFT JOIN locations AS loc ON loc.city_name = c.name
+GROUP BY c.name, loc.street;

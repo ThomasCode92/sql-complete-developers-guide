06f04a75814afd082be2a503e59258a92d4c82e1
ThomasCode92 <thomascode92@hotmail.com>
2023-11-06 17:21:19 +0100

    Introducing LEFT JOIN

--- data/09-queries-with-relations.sql
+++ data/09-queries-with-relations.sql
@@ -12,3 +12,10 @@ FROM events AS e
 INNER JOIN locations AS loc ON e.location_id = loc.id
 INNER JOIN events_users AS eu ON eu.event_id = e.id
 INNER JOIN users AS u ON eu.user_id = u.id;
+
+SELECT * FROM locations AS loc
+LEFT JOIN events AS e ON e.location_id = loc.id;
+
+SELECT * FROM cities AS c
+LEFT JOIN locations AS loc ON loc.city_name = c.name
+LEFT JOIN events AS e ON e.location_id = loc.id;
